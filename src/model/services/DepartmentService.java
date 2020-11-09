67ac24f378453e53bae6e170229fb6779e08d289
unknown <lucasngarcia8@hotmail.com>
2020-11-09 15:40:35 -0300

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -14,4 +14,9 @@ public class DepartmentService {
 		return dao.findAll();
 	}
 	
+	public void saveOrUpdate(Department obj) {
+		if(obj.getId() == null) dao.insert(obj);
+		else dao.update(obj);
+	}
+	
 }
